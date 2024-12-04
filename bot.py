1fdc6b6b4293fcad0db5a4fc7076355478c4e14e
samXd777 <benavidessamuel202@gmail.com>
2024-12-03 18:44:39 -0600

    bot.py

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from model import get_class
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola! Soy el bot {bot.user}!')
+
+@bot.command()
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f"./{attachment.filename}")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+    else:
+        await ctx.send("Olvidaste subir la imagen :(")
+
+
+bot.run("PON TU TOKEN AQUI")
